afbc3156862166c9c41cc46b590e8e4e723f85e3
jennanoh <jennanoh28@gmail.com>
2024-01-14 14:25:51 -0500

    app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import { Container } from 'react-bootstrap/lib/Tab';
-import './bootstrap.css';
 import Header from './Components/Header';
-import Footer from './Components/Footer';
-import HomeScreen from './screens/HomeScreen';
-
+import './bootstrap.css';
 
 function App() {
   return (
-      <>
-        <Header />
-        <main>
-          <Container>
-            <HomeScreen />
-          </Container>
-        </main>
-        <Footer />
-      </>
+
+    <Header />
+
   );
 }
 
