22bacc12ac49aad3623ef21a20ebad4528a8a6d9
jennanoh <jennanoh28@gmail.com>
2024-01-17 17:52:17 -0500

    event cards with img and title created

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import events from '../dummyevents'
+import Events from '../Components/Events'
+import { Col, Row } from 'react-bootstrap'
 
 const HomeScreen = () => {
   return (
     <>
-    <div>HomeScreen</div>
+    <h1>Events in Your Area</h1>
+    <Row>
+      {events.map (e => (
+      <Col sm={12} md={6} lg={4} xl={3}>
+        <Events event={e} />
+      </Col>
+      ))
+      }
+    </Row>
     </>
   )
 }
