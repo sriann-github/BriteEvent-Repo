b31940defec9dd68a5c40f98bbe039d8e859c082
jennanoh <jennanoh28@gmail.com>
2024-01-18 23:14:47 -0500

    Icons.js created

---
+++ frontend/src/Components/Icons.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Card, Row, Col, Container } from 'react-bootstrap'
+
+
+const Icons = () => {
+  return (
+    <>
+    <Container>
+      <Row>
+      <Col>
+        <Card>
+          <Card.Title>
+            Icon
+          </Card.Title>
+        </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Title>
+            Icon
+          </Card.Title>
+         </Card> 
+      </Col>
+      <Col>
+        <Card>
+          <Card.Title>
+            Icon
+          </Card.Title>
+         </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Title>
+            Icon
+          </Card.Title>
+         </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Title>
+            Icon
+          </Card.Title>
+         </Card>
+      </Col>
+      <Col>
+        <Card>
+          <Card.Title>
+            Icon
+          </Card.Title>
+         </Card>
+      </Col>
+      </Row>
+    </Container>
+    </>
+  )
+}
+
+export default Icons
