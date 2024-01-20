fdc2af6e59c061066f67ca21882915f8d318717e
SA <sriann@outlook.com>
2024-01-20 04:46:33 -0800

    Router changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import './bootstrap.css';
 import './index.css';
-//import { Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import HomeScreen from './screens/HomeScreen';
-//import EventScreen from './screens/EventScreen';
+import EventScreen from './screens/EventScreen';
 import { Container } from 'react-bootstrap';
 import React from 'react'
 
@@ -13,17 +13,18 @@ function App() {
   return (
     <>
       <div class="flex-wrapper">
+      <Router>
         <Header />
           <main>
             <Container>
-           {/* <Routes>
-              <Route path='/' exact element= {< HomeScreen />} />
-             <Route path='/event/:id' exact element= {<EventScreen />}/> 
-  </Routes>*/ }
-              < HomeScreen />
+              <Routes>
+                <Route path='/' exact element= {< HomeScreen />} />
+                <Route path='/event/:id' exact element= {<EventScreen />}/> 
+              </Routes> 
             </Container>
           </main>
         <Footer />
+      </Router>
       </div>
     </>
   );
