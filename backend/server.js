3bba8bc67dff9c1b60928d0f218bd81f74d93ad3
jennanoh <jennanoh28@gmail.com>
2024-01-29 16:00:56 -0500

    APIs separated from server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,13 @@
 import express from 'express'
-import events from './data/events.js'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
+import router from './routes/eventRoutes.js'
 
 const app = express()
 dotenv.config()
 connectDB()
 
-//When a request is submitted from the FE for a list of events, it's read here and a json object is returned as events
-app.get('/api/events', (req, res) => {
-  res.json(events)
-})
+app.use('/api/events', router)
 
-app.get('/api/event/:id', (req, res) =>
-{
-  const event = events.find(e => e.id === req.params.id)
-  res.json(event)
-})
-
+app.use(errorHandler)
 app.listen(5000, console.log('Server is running on port 5000'))
