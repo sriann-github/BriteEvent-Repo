36534ab92c533eb5dee0411ea3e06e82ffe36a3d
jennanoh <jennanoh28@gmail.com>
2024-02-08 22:30:30 -0500

    token generation bug

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
  const generateToken = (id) =>{
-  jwt.sign({id: id}, process.env.JWT_SECRET_KEY, {expiresIn: '30d'})
+  return jwt.sign({id: id}, process.env.JWT_SECRET_KEY, {expiresIn: '30d'})
 }
 
 export default generateToken
